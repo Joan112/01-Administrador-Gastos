eda9bda60245e61d8ff0bd8cfc8fc882f949b1b3
joan.iribe <joan.iribe@coppel.com>
2025-01-23 10:52:41 -0700

    prueba codeQL

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-// import React from 'react';
-import { useState, useEffect } from 'react';
-import ControlPresupuesto from './components/ControlPresupuesto';
-import Formulario from './components/Formulario';
-import LIstado from './components/Listado';
-import Pregunta from './components/pregunta';
+// import React from 'react'
+import { useState, useEffect } from 'react'
+import ControlPresupuesto from './components/ControlPresupuesto'
+import Formulario from './components/Formulario'
+import LIstado from './components/Listado'
+import Pregunta from './components/pregunta'
 
-
-
-
-function App() {
+function App () {
 
   // definimos el state
   const [presupuesto, setPresupuesto] = useState(0)
@@ -18,62 +15,45 @@ function App() {
   const [gasto, setGasto] = useState({})
   const [crearGasto, setCrearGasto] = useState(false)
 
- useEffect(() => {
+  useEffect(() => {
     if (crearGasto) {
-
       setGastos([
         ...gastos,
         gasto
       ])
 
-      const presupuestoRestante = restatnte -gasto.cantidad
+      const presupuestoRestante = restatnte - gasto.cantidad
       setRestante(presupuestoRestante)
 
       setCrearGasto(false)
     }
- }, [gasto]);
+  }, [gasto])
 
- 
+
 
   return (
-    <div className="container">
+    <div className='container'>
       <header>
         <h1>Gasto semanal</h1>
-
-        <div className="contenido-principal contenido">
-          {mostrarpregunta ? 
-          (
-            <Pregunta
-              setPresupuesto={setPresupuesto}
-              setRestante={setRestante}
-              setMostrarpregunta={setMostrarpregunta}
-            />
-          ) : (
-              <div className="row">
-                <div className="one-half column">
-                  <Formulario
-                    setGasto= {setGasto}
-                    setCrearGasto={setCrearGasto}
-                  />
-                </div>
-                <div className="one-half column">
-                  <LIstado
-                    gastos={gastos}
-                  />
-
-                  <ControlPresupuesto 
-                    presupuesto={presupuesto}
-                    restatnte={restatnte}
-                  />
-                </div>
-              </div>
-            )
-          }
-
+        <div className='contenido-principal contenido'>
+          {mostrarpregunta ?
+             (
+             <Pregunta setPresupuesto={setPresupuesto} setRestante={setRestante} setMostrarpregunta={setMostrarpregunta} />
+             ) : (
+             <div className='row'>
+               <div className='one-half column'>
+                 <Formulario setGasto={setGasto} setCrearGasto={setCrearGasto} />
+               </div>
+               <div className='one-half column'>
+                 <LIstado gastos={gastos} />
+                 <ControlPresupuesto presupuesto={presupuesto} restatnte={restatnte} />
+               </div>
+             </div> // solo vamos a poner un comentario nada mas
+             )}
         </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
